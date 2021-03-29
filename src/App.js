f78869e1537f0dd5f8d8991a070f983fd3bc374e
Antonio <aantonio_94@hotmail.com>
2021-03-28 18:43:43 -0600

    asda

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import './App.css';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, HashRouter } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Projects from "./pages/Projects";
@@ -12,19 +12,19 @@ function App() {
   return (
 
     <div className="App">
-	    <Router>
+	    <HashRouter basename="/">
 		    <Header />
 		   		<Switch>
 		   		  <Route exact path = "/">
 		   		  	<About />
 		   		  </Route>
-		          <Route exact path="/about">
+		          <Route path="/about">
 		            <About />
 		          </Route>
-		          <Route exact path="/projects">
+		          <Route path="/projects">
 		            <Projects />
 		          </Route>
-		          <Route exact path="/contact">
+		          <Route path="/contact">
 		            <Contact />
 		          </Route>
 		        </Switch>
